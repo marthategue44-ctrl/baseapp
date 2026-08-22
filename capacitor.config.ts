0a0fc140260fb5a88653305e2b81e807d669d5ec
marthategue44-ctrl <marthategue44@gmail.com>
2026-08-22 11:28:25 -0500

    Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'EmpresaApp',
+  webDir: 'www'
+};
+
+export default config;
